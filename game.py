2826b307e0e5519f2ef542d263162db215885287
[Yuhan <[lyuhan322@gmail.com]>
2023-08-25 14:52:07 +0930

    update some funcion in game.py

--- game.py
+++ game.py
@@ -7,10 +7,10 @@ class Game:
         self.number = self.player_number()
         self.attempts = 0
 
-    def player_number(self):
+    def player_number(self): # four digit
         return str(random.randint(0000, 9999))
 
-    def guess(self, guess):
+    def guess(self, guess):# check the guess and give the hint
         answer = []
         for i in range(4):
             if guess[i] == self.number[i]:
@@ -19,7 +19,8 @@ class Game:
                 answer.append("x")
 
         return ''.join(answer)
-    def quit_game(self,player_input):
+
+    def quit_game(self,player_input):#quit the game
         return player_input == 'N'
 
 
@@ -35,7 +36,7 @@ class Game:
 
 
         while True:
-            if self.attempts >=1:
+            if self.attempts >=1: # ask fot quit every time
                 again = input("Do you want to continue to guess?(Y/N)")
                 check_quit = self.quit_game(again)
                 if again == "N":
@@ -43,16 +44,17 @@ class Game:
                     print("Thanks for playing! Hope to see you again!")
                     break
 
+            # som expect error: input number <4 and input is not numberic
             guess_number = input("Enter your guess: ")
             self.attempts += 1
             if not guess_number.isnumeric():
                 print("Please enter the number! ")
                 continue
             if len(guess_number) != 4:
-                print("Please enter four digit!")
+                print("Please enter four digits!")
                 continue
 
-            check_answer = self.guess(guess_number)
+            check_answer = self.guess(guess_number) #correct answer
             if check_answer == "oooo":
                 print(" -----------------")
                 print("|                 |")
@@ -61,11 +63,16 @@ class Game:
                 print(" ------------------")
                 print("You got the correct answer!")
                 print("You have guessed the number in {} attempts.".format(self.attempts))
-                print("Thanks for playing!")
-                break
+
+                play_agian = input("Do you want to play it again?(Y/N)") # ask  try gain?
+                if play_agian == "Y":
+                    Game().play()
+                else:
+                    print("Thanks for playing!")
+                    break
             if check_answer =='':
                 continue
-            else:
+            else:# hint
                 print("Hint: {}".format(check_answer))
                 print(" 'o' means that the number is correct and is in the right spot")
                 print(" 'x' means that the number is correct and buis in the wrong spot")
@@ -74,4 +81,3 @@ class Game:
 if __name__ == "__main__":
     game = Game()
     game.play()
-
